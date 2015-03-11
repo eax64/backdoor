08ca8bc1030290bbb242471788d4e4bbfc3501f3
Kevin Soules <inceax64@gmail.com>
2015-03-11 22:58:00 +0100

    empty test.c

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 ** Login   <soules_k@epitech.net>
 ** 
 ** Started on  Wed Mar 11 22:17:43 2015 eax
-** Last update Wed Mar 11 22:43:24 2015 eax
+** Last update Wed Mar 11 22:57:42 2015 eax
 */
 
 #include <err.h>
@@ -14,18 +14,7 @@
 
 int	main(int ac, char **av)
 {
-  user_t user;
-  group_t groups;
-  access_request_t req;
-
-  if (ac != 3)
-    errx(EXIT_FAILURE, "Usage: %s user password", av[0]);
-
-  req.name = av[1];
-  req.password = av[2];
-
-  if (auth(&user, &groups, &req) == -1)
-    errx(EXIT_FAILURE, "Can't authenticate");
-  
+  (void) ac;
+  (void) av;
   return 0;
 }
